554b74a7672d39e88ea7a1995596e6f56a3d3527
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-07-14 21:45:27 -0300

    finalizando seção sobre regex

--- expressoes_regulares/script.js
+++ expressoes_regulares/script.js
@@ -61,24 +61,41 @@ console.log('regex de negação com alfanumericos ' + negaExpressaoAlfanumerica.
 console.log('regex de negação com espaços ' + negaExpressaoEspaco.test(" ")) // false porque tem espaços. mas se adicionarmos letras, por exemplo, passa a ser true
 console.log("=============================================")
 
+///// Regex com caracteres especiais /////
+//Podemos utilizar os caracteres especiais de verias formas
+console.log("==============Caracter especial=============")
+let ano = /\d\d\d\d/
+console.log(ano.test('05')) //false - não tem digitos suficientes
+console.log(ano.test('2019')) //true - tem o suficiente
+console.log(ano.test('11092001')) // true - tem o suficiente (e excede)
+console.log(ano.test('opa')) // false
+console.log("=============================================")
 
-//** verificar  */Para incluir um caracter especial (como por exemplo as aspas (), arroba@, hashtags#, etc  ) na verificação como algo que o usuario vai digitar, ou algo que precisa estar dentro da string da variavel , você precisa usar a barra invertida \ antes desse caracter. Segue o exemplo
+let palavraTresLetras = /\w\w\w/
+console.log(palavraTresLetras.test('bom')) //true
+console.log(palavraTresLetras.test('dia')) //true
+console.log(palavraTresLetras.test('oi')) //false
+console.log(palavraTresLetras.test('teste')) //true
 
-let regexComCaracterEspecial =/\@/
-let fraseComCaracterEspecial = '@pamelalima'
-console.log('frase com caracter especial ' + regexComCaracterEspecial.test(fraseComCaracterEspecial))
+//Para incluir um caracter especial (como por exemplo as aspas () na verificação como algo que o usuario vai digitar, ou algo que precisa estar dentro da string da variavel , você precisa usar a barra invertida \ antes desse caracter. Segue o exemplo
+
+let regexComCaracterEspecial =/\(\w+\)/
+console.log('frase com caracter especial ' + regexComCaracterEspecial.test("(palavra)")) //true, porque tem incluiu o parenteses
+console.log('frase com caracter especial ' + regexComCaracterEspecial.test("sem parenteses")) //false, porque nao tem parenteses
 
 
-
-
-
-
-
-
+///// Operador plus + /////
+//Quando um + está apo´s alguma expressão, este pode se repetir mais de uma vez
+console.log("============= Operador plus + ===============")
+let muitosOuPoucosDigitos = /\d+/
+console.log(muitosOuPoucosDigitos.test('123')) //true
+console.log(muitosOuPoucosDigitos.test('123654')) //true
+console.log(muitosOuPoucosDigitos.test('')) //false - tem que haver pelo menos um digito
 
 
 //O operador ? funciona para determinar que aquele caracter(independente do tipo) é opcional, ou seja, quando o regexp foi instanciado, se o resto da expressão estiver correta mas aquele caracter nao, tudo bem porque ele é opcional
 //Esse operador impoe sua regra no caracter que vem antes dele. no exemplo a seguir, vai sobrecair na letra 'a' da palavra 'pamela'. Note que mesmo lendo a variavel que não contem o ultimo 'a' ainda retorna true. Porque esse caracter é opcional
+console.log("============= Operador ? ===============")
 let nomeOpcional = 'pamel4'
 let regexOpcional = new RegExp('pamela?')
 console.log(`esse regexp funcionou ${regexOpcional.test(nomeOpcional)}`)
@@ -95,3 +112,67 @@ console.log(
     fraseDiferente
   )}`
 )
+
+///// Ococrrência precisa /////
+//Inserir o número de ocorrencia entre {}
+console.log("========= Ococrrência precisa ===========")
+
+let telefone = /\d{4,5}-\d{4}/
+console.log(telefone.test('4404-5050'))//true 
+console.log(telefone.test('99999-9999'))//true
+console.log(telefone.test('999-999'))//false
+console.log(telefone.test('9999-9'))//false- cumpriu a primeira expressão, mas nao cumpriu a segunda
+
+///// Metodo exec /////
+//O metodo exec nos retorna um objeto com algumas infromações sobre a regex
+//Se nada for encontrado, returna null
+
+let teste = /\d+/.exec("O numero 100")
+console.log(teste)
+console.log(teste.index)
+  
+///// Metodo match /////
+//O metodo match funciona de forma similar ao exec, porém com uma sintaxe diferente
+
+let teste2 = 'O numero 200'.match(/\d+/)
+console.log(teste2)
+console.log(teste2.index)
+
+///// Choice pattern /////
+//Podemos colocar uma instrução na regex que funciona com um || (or) das condicionais
+//Primeiro colocamos as opções dentro de um parenteses e as dividimos usando o operador ||. Como no exemplo abaixo
+
+console.log("========= Choice pattern ===========")
+let frutas = /\d+ (bananas|maçãs|laranjas)/
+console.log(frutas.test('10 bananas'))//true
+console.log(frutas.test('25 batatas'))//false
+console.log(frutas.test('8 laranjas'))//true
+
+let pessoa = /\w+: (Matheus|Pedro|João|Maria)/
+console.log(pessoa.test('nome: Maria')) //true
+
+///// Na pratica: validando um dominio /////
+//Da seguinte forma, podemos validar um dominio:
+console.log("========= validando um dominio ===========")
+let validarDominio = /[www\.]?\w+(\.com|\.com\.br)/ //o barra invertida antes dos pontos sinalizando que eles devem ser escritos
+console.log(validarDominio.test("www.google.com")) //true
+console.log(validarDominio.test("www.google.br")) //false
+console.log(validarDominio.test("teste.com")) //false
+
+///// Na pratica: validando e-mail /////
+//da seguinte forma, podemos validar um e-mail
+console.log("========= validando um e-mail ===========")
+let validarEmail = /\w+@\w+\.\w+/
+console.log(validarEmail.test('pamela@hotmail.com')) //true
+console.log(validarEmail.test('pamela@hotmail')) //false
+console.log(validarEmail.test('pamela.hotmail.com')) //false
+
+///// Na pratica: validando data de nascimento /////
+//da seguinte forma, podemos validar uma data de nascimento
+console.log("===== validando uma data de nascimento =====")
+let validarDataNasc = /^[0-9]{2}[/][0-9]{2}[/][0-9]{4}/
+console.log(validarDataNasc.test('25/12/1990')) //true
+console.log(validarDataNasc.test('25/1/2016')) //false
+console.log(validarDataNasc.test('23/04/2008')) //true
+console.log(validarDataNasc.test('25-12-1990')) //false
+
